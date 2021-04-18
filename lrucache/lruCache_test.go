44a242389921af3deb0797686f3b84811dc38b66
Ryan Cerankowski <rcerankowski@gmail.com>
2021-04-18 13:17:32 -0400

    Updated test cases
    Cleaned up test cases to follow a more idiomatic and modular approach

--- lrucache/lruCache_test.go
+++ lrucache/lruCache_test.go
@@ -5,63 +5,84 @@ import (
 	"testing"
 )
 
+type pair struct {
+	key, value int
+}
+
+var testCases = []struct {
+	name     string
+	capacity int
+	input    []pair
+	expected []pair
+}{
+	{name: "standard input",
+		capacity: 2,
+		input: []pair{
+			{1, 1},
+			{2, 2},
+			{3, 3},
+			{4, 4}},
+		expected: []pair{
+			{1, -1},
+			{2, -1},
+			{3, 3},
+			{4, 4}}},
+	{name: "overwrite value",
+		capacity: 3,
+		input: []pair{
+			{1, 1},
+			{2, 1},
+			{3, 3},
+			{4, 4}},
+		expected: []pair{
+			{1, -1},
+			{2, 1},
+			{3, 3},
+			{4, 4}}},
+	{name: "capacity 1",
+		capacity: 1,
+		input: []pair{
+			{1, 1},
+			{2, 2},
+			{3, 3},
+			{4, 4}},
+		expected: []pair{
+			{1, -1},
+			{2, -1},
+			{3, -1},
+			{4, 4}}},
+	{name: "capacity 2 overwrite value",
+		capacity: 2,
+		input: []pair{
+			{2, 1},
+			{1, 1},
+			{2, 3},
+			{4, 1}},
+		expected: []pair{
+			{1, -1},
+			{2, 3},
+			{4, 1}}},
+	{name: "capacity 2",
+		capacity: 2,
+		input: []pair{
+			{2, 1},
+			{3, 1},
+			{4, 3}},
+		expected: []pair{
+			{2, -1},
+			{3, 1},
+			{4, 3}}},
+}
+
 func TestLRUCache(t *testing.T) {
-	obj := Constructor(2)
-	obj.Put(1, 1)
-	obj.Put(2, 2)
-	obj.Put(3, 3)
-	obj.Put(4, 4)
-
-	assert.Equal(t, -1, obj.Get(1))
-	assert.Equal(t, -1, obj.Get(2))
-	assert.Equal(t, 3, obj.Get(3))
-	assert.Equal(t, 4, obj.Get(4))
-}
-
-func TestLRUCacheOverwriteValue(t *testing.T) {
-	obj := Constructor(3)
-	obj.Put(1, 1)
-	obj.Put(1, 2)
-	obj.Put(3, 3)
-	obj.Put(4, 4)
-
-	assert.Equal(t, 2, obj.Get(1))
-	assert.Equal(t, 3, obj.Get(3))
-	assert.Equal(t, 4, obj.Get(4))
-}
-
-func TestLRUCacheCapacity1(t *testing.T) {
-	obj := Constructor(1)
-	obj.Put(1, 1)
-	obj.Put(2, 2)
-	obj.Put(3, 3)
-	obj.Put(4, 4)
-
-	assert.Equal(t, -1, obj.Get(1))
-	assert.Equal(t, -1, obj.Get(2))
-	assert.Equal(t, -1, obj.Get(3))
-	assert.Equal(t, 4, obj.Get(4))
-}
-
-func TestLRUCacheCapacity2Overwrite(t *testing.T) {
-	obj := Constructor(2)
-	obj.Put(2, 1)
-	obj.Put(1, 1)
-	obj.Put(2, 3)
-	obj.Put(4, 1)
-
-	assert.Equal(t, -1, obj.Get(1))
-	assert.Equal(t, 3, obj.Get(2))
-	assert.Equal(t, 1, obj.Get(4))
-}
-
-func TestLRUCacheCapacity2(t *testing.T) {
-	obj := Constructor(2)
-	obj.Put(2, 1)
-	obj.Put(3, 2)
-	obj.Put(4, 3)
-
-	assert.Equal(t, -1, obj.Get(2))
-	assert.Equal(t, 2, obj.Get(3))
-	assert.Equal(t, 4, obj.Get(4))
+	for _, test := range testCases {
+		t.Logf("Running test: %q", test.name)
+		obj := Constructor(test.capacity)
+		for _, input := range test.input {
+			obj.Put(input.key, input.value)
+		}
+		for _, output := range test.expected {
+			assert.Equal(t, output.value, obj.Get(output.key))
+		}
+	}
 }
